af59274279c7e57dea455379c625d8c628754c28
terrell linnell <terrelllinnell@terrells-MacBook-Pro.local>
2016-12-10 15:32:59 -0700

    I have a working code

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+var mapToArray = function (arr) {
 
+  for(var i = 0; i<arr.length; i++){
+
+    $('.table').append('<tr> <td>' + arr[i].city + '</td> <td>' + arr[i].state + '</td> <td>' + arr[i].zip + '</td> </tr>');
+
+  };
+
+};
+
+
+
+var dataLoad = function(){
+
+$('.table').append('<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>');
+setTimeout(function () {
+  $.ajax({
+    url:"https://api.meetup.com/2/cities",
+    type: 'GET',
+    success: function(data) {
+$('i').css("display", "none");
+  mapToArray(data.results);
+
+    },
+    dataType: 'jsonp'
+  })
+}, 1500);
+
+}
+
+$('.dataLoad').click(dataLoad);
