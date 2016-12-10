19ad566117b8b4c83ca404439df7c6e4133f6889
terrell linnell <terrelllinnell@terrells-MacBook-Pro.local>
2016-12-10 14:34:34 -0700

    a list of cities

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# introTojQuery
+# citiesAjaxExample
--- main.js
+++ main.js
@@ -1,17 +1,1 @@
 
-// var greeting = function() {
-//
-//   $('.jumbo_text').text('I changed the text!').css('background-color', 'orange');
-// };
-//
-// $(".alert_button").click(greeting);
-
-var getName = function(){
-  var nameInput = $('.name_input').val();
-  console.log(nameInput)
-  alert("Hello there " + nameInput);
-};
-
-$('.alert_button2').click(getName);
-
-console.log("ftgyhjhgf");
